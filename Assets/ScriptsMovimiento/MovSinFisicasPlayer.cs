cdf83b0babd3b4abb8631e20e62e9aa1e88750a7
Edil Yael <edilbertoyaelperezlopez1@gmail.com>
2025-10-27 18:58:40 -0600

    Implementacion AnimJugador
    segunda implementacion

--- Assets/ScriptsMovimiento/MovSinFisicasPlayer.cs
+++ Assets/ScriptsMovimiento/MovSinFisicasPlayer.cs
@@ -5,10 +5,13 @@ public class MovSinFisicasPlayer : MonoBehaviour
     [SerializeField] private float velocidad = 3f;
     private Vector3 forward, right;
     private CharacterController controller;
+    private Animator animacion; 
+    private float velocidadRotacion = 15f;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        controller = GetComponentInChildren<CharacterController>();
+        animacion = GetComponentInChildren<Animator>();
 
         forward = Camera.main.transform.forward;
         forward.y = 0;
@@ -25,30 +28,48 @@ public class MovSinFisicasPlayer : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direccion = Vector3.zero;
+        bool movimiento = false; 
     
         //W
         if (vertical > 0) 
         {
             direccion = (forward + right).normalized;
+            movimiento = true;
         }
         //A
         else if (horizontal < 0)
         {
             direccion = (forward - right).normalized;
+            movimiento = true;
         }
         //S
         else if (vertical < 0)
         {
             direccion = (-forward - right).normalized;
+            movimiento = true;
         }
         //D
         else if (horizontal > 0)
         {
             direccion = (-forward + right).normalized;
+            movimiento = true;
+        }
+        // 
+        controller.Move(direccion * velocidad * Time.deltaTime);
+
+
+        if (movimiento)
+        {
+            // Calcula la rotación que haría que el modelo mire en la dirección de movimiento
+            Quaternion rotacionDeseada = Quaternion.LookRotation(direccion);
+            // Aplica la rotación suavemente (Slerp) para evitar que gire de golpe
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotacionDeseada, velocidadRotacion * Time.deltaTime);
+        }
+
+        if(animacion != null){
+            animacion.SetBool("caminar",movimiento);
         }
 
 
-        // 
-        controller.Move(direccion * velocidad * Time.deltaTime);
     }
 }
